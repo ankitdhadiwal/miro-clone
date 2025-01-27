b201638c2591b2812fa5eca27a3bb94e078925e1
ankitdhadiwal <ankit.dhadiwal.885@gmail.com>
2025-01-27 15:29:16 +0530

    added room participants

---
+++ app/board/[boardId]/_components/user-avatar.tsx
@@ -0,0 +1,29 @@
+import {Hint} from "@/components/hint";
+import { Avatar, AvatarFallback, AvatarImage, AvatarProps} from "@radix-ui/react-avatar";
+
+interface UserAvatarProps {
+    src?: string;
+    name?: string;
+    fallback?: string;
+    borderColor?: string;
+};
+
+export const UserAvatar = ({
+    src,
+    name,
+    fallback,
+    borderColor
+}: UserAvatarProps) => {
+    return (
+        <Hint label={name || "Teammate"} side="bottom" sideOffset={18}>
+            <Avatar className="h-8 w-8 border-2 rounded-full"
+              style={{ borderColor }}
+              >
+                <AvatarImage src={src} className="rounded-full"/>
+                <AvatarFallback className="text-xs font-semibold">
+                    {fallback}
+                </AvatarFallback>
+            </Avatar>
+        </Hint>
+    );
+};
